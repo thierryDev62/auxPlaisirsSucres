1109eb34158cfa3b1313c3b5236f98a5ea5a2d07
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-10-22 10:09:57 +0200

    mise à jour ajax

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -1,8 +1,6 @@
 $(function() {
-
     $('#categorie').click(function() {
         var temp = $('#categorie option:selected').val();
         $('#afficheCategorie').load('includes/code/' + temp + '.html');
-
     });
 });
