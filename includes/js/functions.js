0e9a6a4e6f870bb10144495feba8f96cebf914df
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-10-22 09:37:56 +0200

    mise à jour ajax

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -1,5 +1,5 @@
 $(function() {
-    $("#categorie").trigger('click');
+
     $('#categorie').click(function() {
         var temp = $('#categorie option:selected').val();
         $('#afficheCategorie').load('includes/code/' + temp + '.html');
