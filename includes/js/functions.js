a802341651c55141c55e98224163bf83e57b482d
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-10-22 08:39:36 +0200

    Update functions.js

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -1,6 +1,7 @@
 $(function() {
-    $('#categorie').trigger('click')(function() {
+    $('#categorie').click(function() {
         var temp = $('#categorie option:selected').val();
         $('#afficheCategorie').load('includes/code/' + temp + '.html');
+        return false;
     });
 });
