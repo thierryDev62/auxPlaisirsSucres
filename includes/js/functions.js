62d5f240adbff100c53623d528d1cab67bb8fbf6
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-10-23 16:18:13 +0200

    Grosse mise à jour

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -1,6 +1,13 @@
 $(function() {
+    // Ajax sur l'affichage des différentes catégories
     $('#categorie').change(function() {
         var temp = $('#categorie option:selected').val();
         $('#afficheCategorie').load('includes/code/' + temp + '.html');
+    });
+
+    // Lightbox de la galerie
+    $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
     });
 });
