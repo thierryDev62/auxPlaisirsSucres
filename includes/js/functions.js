7aced76412512d327b0b0c38fb900cd7d2b7f52b
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-10-23 16:33:38 +0200

    Update functions.js

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -2,7 +2,6 @@ $(function() {
     // Ajax sur l'affichage des différentes catégories
     $('#categorie').change(function() {
         var temp = $('#categorie option:selected').val();
-        $('#afficheCategorie').html('<div class="spinner-border" role="status"></div>');
         $('#afficheCategorie').load('includes/code/' + temp + '.html');
     });
 
