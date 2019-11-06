40ee892249d7b9030a151e51de5a14e67d51007e
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-11-06 14:35:38 +0100

    Update functions.js

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -1,6 +1,7 @@
 $(function() {
-    // Ajax sur l'affichage des différentes catégories avec un fondu
+    // Ajax sur l'affichage des différentes catégories avec un fondu et un spinner d'attente de chargement
     $('#categorie').change(function(){
+        $('#afficheCategorie').html('<div class="row justify-content-center"><div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div></div>');
         let temp = $('#categorie option:selected').val();
         let box = $('#afficheCategorie');
         box.hide().load('includes/code/' + temp + '.html', function() {
