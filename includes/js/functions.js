36f92842a4a8a50804482d6a09cb012438e03e7a
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-11-08 18:08:47 +0100

    correction bug dans tarif et galerie

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -2,14 +2,26 @@ $(function() {
     // Ajax sur l'affichage des différentes catégories avec un fondu et un spinner d'attente de chargement
     $('#categorie').change(function(){
         $('#afficheCategorie').html('<div class="row justify-content-center"><div class="spinner-border" role="status"><span class="sr-only">chargement...</span></div></div>');
-        let temp = $('#categorie option:selected').val();
-        let box = $('#afficheCategorie');
+        let temp = $('#categorie option:selected').val(); // On récupère la valeur de l'option du select
+        let box = $('#afficheCategorie'); // Variable pour initialiser le fondu
         setTimeout(function(){
+            // Recharge la page par défaut (en AJAX) en cas de clic sur 'Séléctionnez une catégorie'
+            if(temp == 'page-tarifs' || temp == 'page-galerie') {
+
+                box.hide().load('includes/code/' + temp + '.html', function() {
+                    box.fadeIn('750');
+                    return false;
+                });
+
+            } else {
+
         box.hide().load('includes/code/' + temp + '.html', function() {
             box.fadeIn('750');
             return false;
         });
-        }, 500);
+        
+    }}, 500);
+    
     });
    
     // Lightbox de la galerie
