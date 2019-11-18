f072b8bf0ade1f35def0f26a0f51985a026b5fe9
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-11-18 15:27:32 +0100

    mise à jour de la galerie
    Optimisation du code

--- includes/js/affiche-categories.js
+++ includes/js/affiche-categories.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
         // Affichage du spinner bootstrap
         $('#afficheCategorie').html('<div class="row justify-content-center"><div class="spinner-border" role="status"><span class="sr-only">chargement...</span></div></div>');
-        
+
         let temp = $('#categorie option:selected').val(); // On récupère la valeur de l'option du select dans la variable temp
         let box = $('#afficheCategorie'); // Variable pour initialiser le fondu
 
@@ -25,19 +25,64 @@ $(document).ready(function () {
 
             } else {
 
-                box.hide().load('includes/code/' + temp + '.html', function () {
-                    box.fadeIn('750');
-                    return false;
-                });
+                // Test si on choisi une catégorie dans la galerie
+                if (temp == 'cupcakes' || temp == 'entremet' || temp =='gateau-pate-a-sucre' || temp == 'layer-cake' || temp == 'macarons' || temp =='number-letter-cake') {
+                    
+                    // Chargement de la page HTML avec un fondu en fonction de l'option sélectionnée dans la galerie photo
+                    box.hide().load('includes/code/trame-photos.html', function () {
+                        box.fadeIn('750');
 
+                        // Fonction d'affichage de la galerie photos
+                        /******************************************************************************/
+                        $(function () {
+                            let urlImage = "";
+                            switch (temp) {
+
+                                case 'cupcakes':
+                                    urlImage = "galerie-photos/cupcakes/cup-cake-";
+                                    break;
+                                case 'entremet':
+                                    urlImage = "galerie-photos/entremet/entremet-";
+                                    break;
+                                case 'gateau-pate-a-sucre':
+                                    urlImage = "galerie-photos/gateau-pate-a-sucre/gateau-pate-a-sucre-";
+                                    break;
+                                case 'layer-cake':
+                                    urlImage = "galerie-photos/layer-cake/layer-cake-";
+                                    break;
+                                case 'macarons':
+                                    urlImage = "galerie-photos/macarons/macaron-";
+                                    break;
+                                case 'number-letter-cake':
+                                    urlImage = "galerie-photos/number-letter/number-letter-";
+                                    break;
+                            }
+                            // Boucle pour l'affichage de 9 images
+                            for (let numeroImage = 1; numeroImage <= 9; numeroImage++) {
+                                $('#afficheGaleriePhotos').append('<a href="' + urlImage + numeroImage + '.jpg" data-toggle="lightbox" data-gallery="gallery" class="col-md-3"><img src="' + urlImage + numeroImage + '-thumb.jpg" alt="Photo ' + urlImage + ' de cupcakes" class="img-thumbnail shadow w-25 mb-3"></a>');
+                            }
+                        });
+
+                        /******************************************************************************/
+
+                    });
+
+                } else {
+
+                    // Chargement de la page HTML des différents tarifs choisis en option
+                    box.hide().load('includes/code/' + temp + '.html', function () {
+                        box.fadeIn('750');
+                    });
+
+                }
                 // On lance la fonction qui va chercher les tarifs
                 /******************************************************************************/
-                
-                $(function() {
+
+                $(function () {
                     // Test de la catégorie pour l'affichage des prix avec un switch
                     let urlJson = "";
 
-                    switch(temp) {
+                    switch (temp) {
                         case 'gateaux':
                             urlJson = 'json/tarifs-patisserie.json';
                             break;
@@ -63,7 +108,6 @@ $(document).ready(function () {
                 /*******************************************************************************/
             }
         }, 500); // Fin du setTimeout()
-
     });
 });
 
--- includes/js/lightbox.js
+++ includes/js/lightbox.js
@@ -1,12 +1,14 @@
-// Lightbox est un plugin récupéré
-
 $(document).ready(function () {
-    // Lightbox de la galerie
+
+    // Lightbox de la galerie (un plugin récupéré)
     $(document).on("click", '[data-toggle="lightbox"]', function (event) {
         event.preventDefault();
         $(this).ekkoLightbox({
             alwaysShowClose: true,
         });
     });
-
 });
+
+
+
+
