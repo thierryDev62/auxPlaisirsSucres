7f0218031f897f0076f16b152e9d0e927ad536ad
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-11-19 12:30:09 +0100

    Update affiche-categories.js

--- includes/js/affiche-categories.js
+++ includes/js/affiche-categories.js
@@ -1,29 +1,29 @@
 $(function () {
 
-    // Instructions en Ajax pour effectuer l'affichage des différentes catégories avec un fondu et un spinner d'attente de chargement
+    // Instructions en Ajax pour effectuer l'affichage des différentes catégories (tarifs et galerie photos) avec un fondu et un spinner d'attente de chargement
 
-    $('#categorie').change(function () {
+    $('#categorie').change(function () { // Fonction majeure pour la sélection d'une option
 
-        // Affichage du spinner bootstrap
+        // Affichage du spinner bootstrap au cas où la réponse AJAX se fait attendre
         $('#afficheCategorie').html('<div class="row justify-content-center"><div class="spinner-grow" role="status"><span class="sr-only">chargement...</span></div></div>');
 
         let valeurOption = $('#categorie option:selected').val(); // On récupère la valeur de l'option du select dans la variable valeurOption
         let containerPourFondu = $('#afficheCategorie'); // Variable pour initialiser le fondu
 
-        // Recharge la page par défaut en cas de clic sur 'Séléctionnez une catégorie'
+        // Recharge la page par défaut tarifs.html ou galerie.html en cas de clic sur 'Séléctionnez une catégorie'
         if (valeurOption == 'page-tarifs' || valeurOption == 'page-galerie') {
 
             if (valeurOption == 'page-tarifs') {
-                let url = "tarifs.html";
-                $(location).attr('href', url);
+                let urlDefaut = "tarifs.html";
+                $(location).attr('href', urlDefaut);
             } else {
-                let url = "galerie.html";
-                $(location).attr('href', url);
+                let urlDefaut = "galerie.html";
+                $(location).attr('href', urlDefaut);
             }
 
         } else {
 
-            // Test si on choisi une catégorie dans la galerie
+            // Test si on choisi une catégorie dans la galerie photos
             if (valeurOption == 'cupcakes' || valeurOption == 'entremet' || valeurOption == 'gateau-pate-a-sucre' || valeurOption == 'layer-cake' || valeurOption == 'macarons' || valeurOption == 'number-letter-cake') {
 
                 // Chargement de la page HTML avec un fondu en fonction de l'option sélectionnée dans la galerie photo
@@ -33,41 +33,43 @@ $(function () {
                     // Instructions d'affichage de la galerie photos
                     /******************************************************************************/
 
-                    let urlImage = "";
-                    let nombreImages = 9; // Nombre d'images par page
+                    let urlPhoto = "";
+                    let nombrePhotos = 9; // Nombre de photos par page
                     let theme = ""; // Va rajouter le thème dans le titre
+
+                    // Test du type de l'option choisie avec un switch pour affichage des photos
                     switch (valeurOption) {
 
                         case 'cupcakes':
-                            urlImage = "galerie-photos/cupcakes/cup-cake-";
+                            urlPhoto = "galerie-photos/cupcakes/cup-cake-";
                             theme = "sur le thème des cupcakes";
                             break;
                         case 'entremet':
-                            urlImage = "galerie-photos/entremet/entremet-";
+                            urlPhoto = "galerie-photos/entremet/entremet-";
                             theme = "sur le thème des entremets";
                             break;
                         case 'gateau-pate-a-sucre':
-                            urlImage = "galerie-photos/gateau-pate-a-sucre/gateau-pate-a-sucre-";
+                            urlPhoto = "galerie-photos/gateau-pate-a-sucre/gateau-pate-a-sucre-";
                             theme = "sur le thème des gâteaux à pâte à sucre";
                             break;
                         case 'layer-cake':
-                            urlImage = "galerie-photos/layer-cake/layer-cake-";
+                            urlPhoto = "galerie-photos/layer-cake/layer-cake-";
                             theme = "sur le thème des layer cakes";
                             break;
                         case 'macarons':
-                            urlImage = "galerie-photos/macarons/macaron-";
+                            urlPhoto = "galerie-photos/macarons/macaron-";
                             theme = "sur le thème des macarons";
                             break;
                         case 'number-letter-cake':
-                            urlImage = "galerie-photos/number-letter/number-letter-";
+                            urlPhoto = "galerie-photos/number-letter/number-letter-";
                             theme = "sur le thème des number ou letter cakes";
                             break;
                     }
                     $('#themePhoto').append(theme); // Affichage du thème dans le titre
 
-                    // Boucle pour l'affichage de 9 images
-                    for (let numeroImage = 1; numeroImage <= nombreImages; numeroImage++) {
-                        $('#afficheGaleriePhotos').append('<a href="' + urlImage + numeroImage + '.jpg" data-toggle="lightbox" data-gallery="gallery" class="col-md-3"><img src="' + urlImage + numeroImage + '-thumb.jpg" alt="Photo ' + urlImage + ' de cupcakes" class="img-thumbnail shadow w-25 mb-3"></a>');
+                    // Boucle pour effectuer l'affichage des photos où nombrePhotos représente le nombre de photos à afficher
+                    for (let numeroPhoto = 1; numeroPhoto <= nombrePhotos; numeroPhoto++) {
+                        $('#afficheGaleriePhotos').append('<a href="' + urlPhoto + numeroPhoto + '.jpg" data-toggle="lightbox" data-gallery="gallery" class="col-md-3"><img src="' + urlPhoto + numeroPhoto + '-thumb.jpg" alt="Photo ' + urlPhoto + ' de cupcakes" class="img-thumbnail shadow w-25 mb-3"></a>');
                     }
 
                     /******************************************************************************/
@@ -75,34 +77,32 @@ $(function () {
 
             } else {
 
-                // Chargement de la page HTML des différents tarifs choisis en option
+                // Sinon on charge la page HTML des différents tarifs choisis en option
                 containerPourFondu.hide().load('includes/code/' + valeurOption + '.html', function () {
                     containerPourFondu.fadeIn('750');
 
                     // On lance les instructions qui vont chercher les tarifs
                     /******************************************************************************/
+                    
+                    let urlJsonTarifs = "";
 
                     // Test de la catégorie pour l'affichage des prix avec un switch
-                    let urlJson = "";
-
                     switch (valeurOption) {
                         case 'gateaux':
-                            urlJson = 'json/tarifs-patisserie.json';
+                            urlJsonTarifs = 'json/tarifs-patisserie.json';
                             break;
                         case 'anniversaire':
-                            urlJson = 'json/tarifs-anniversaire.json';
+                            urlJsonTarifs = 'json/tarifs-anniversaire.json';
                             break;
                         case 'aperitif':
-                            urlJson = 'json/tarifs-aperitif.json';
+                            urlJsonTarifs = 'json/tarifs-aperitif.json';
                             break;
-                        default:
-                            return false;
                     };
 
-                    // Affichage des prix en fonction de la catégorie choisie
-                    $.getJSON(urlJson, function (tarifs) {
+                    // Fonction pour l'affichage des prix en fonction de la catégorie choisie
+                    $.getJSON(urlJsonTarifs, function (tarifs) {
 
-                        // Boucle pour afficher chaque tarif au bon endroit
+                        // Boucle for in pour afficher chaque tarif au bon endroit
                         for (let tarifsId in tarifs) {
                             $(tarifsId).html(tarifs[tarifsId]);
                         }
