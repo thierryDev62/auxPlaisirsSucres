b36f9e1cbaef7bd42e34d0eff52e1a0e95163e27
thierryDev62 <56256915+thierryDev62@users.noreply.github.com>
2019-11-26 13:45:33 +0100

    mise à jour page index

---
+++ includes/js/affiche-modal.js
@@ -0,0 +1,29 @@
+// Méthodes JQuery pour afficher les modals au clic sur les différents boutons
+
+$(function () {
+
+    $('#qui').on('click', function () {
+        $('#modalQui').fadeIn('slow');
+    })
+
+    $('#fermerModalQui').on('click', function () {
+        $('#modalQui').fadeOut('slow');
+    })
+
+    $('#mentions').on('click', function () {
+        $('#modalMentions').fadeIn('slow');
+    })
+
+    $('#fermerModalMentions').on('click', function () {
+        $('#modalMentions').fadeOut('slow');
+    })
+
+    $('#contact').on('click', function () {
+        $('#modalContact').fadeIn('slow');
+    })
+
+    $('#fermerModalContact').on('click', function () {
+        $('#modalContact').fadeOut('slow');
+    })
+
+});
